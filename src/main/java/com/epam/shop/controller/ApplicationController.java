aed72327d82c555b9a0c9c38dbb763f9f5f580ee
pashakohann <71321311+pashakohann@users.noreply.github.com>
2021-12-02 18:36:45 +0300

    only Class

---
+++ src/main/java/com/epam/shop/controller/ApplicationController.java
@@ -0,0 +1,8 @@
+package com.epam.shop.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/shop")
+public class ApplicationController extends HttpServlet {
+}
